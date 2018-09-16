ee040a2ccfac367838763de7e5de7e03f58ade77
sltries <sltries@umich.edu>
2018-09-16 13:32:41 -0400

    merge_sort, quick_sort

---
+++ merge_sort.cpp
@@ -0,0 +1,129 @@
+// INSERTION SORT - cycle through array, inserting elements in correct position
+// swap towards left until element is in the correct spot
+// O(n^2)
+
+#include <iostream>
+#include <vector>
+
+int * merge(int * arr, int l, int m, int r) {
+    
+    
+    std::cout << "ARRAY:\n";
+    for (int i = 0; i < r; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << "\n";
+    
+    int index1 = l;
+    int index2 = m + 1;
+    int index3 = l;
+    
+    std::cout << "l is " << l << "\n";
+    std::cout << "r is " << r << "\n";
+    std::cout << "m is " << m << "\n";
+    
+    int arr_1[m - l + 1];
+    int arr_2[r - m];
+
+    
+    // copy elements
+    for (int i = 0; i < m - l + 1; ++i) {
+        arr_1[i] = arr[i + l];
+    }
+    
+    for (int i = 0; i < r - m - 1; ++i) {
+        arr_2[i] = arr[i + m + 1];
+    }
+    
+    std::cout << "ARRAY L:\n";
+    for (int i = 0; i < m + 1; ++i) {
+        std::cout << arr_1[i] << " ";
+    }
+    
+    std::cout << "\n";
+    
+    std::cout << "ARRAY R:\n";
+    for (int i = 0; i < m + 1; ++i) {
+        std::cout << arr_2[i] << " ";
+    }
+    
+    std::cout << "\n";
+    
+    index1 = 0;
+    index2 = 0;
+    
+    
+    // two finger algorithm
+    while (index1 < m - l + 1 + 1 && index2 < r - m) {
+        if (arr_1[index1] < arr_2[index2]) {
+            arr[index3] = arr_1[index1];
+            ++index1;
+        }
+        
+        if (arr_2[index2] < arr_1[index1]) {
+            arr[index3] = arr_2[index2];
+            ++index2;
+        }
+        
+        ++index3;
+    }
+    
+    // copy remaining elements over
+    for (index1; index1 < m - l + 1; ++index1) {
+        arr[index3] = arr_1[index1];
+        ++index3;
+    }
+    
+    for (index2; index2 < r - m; ++index2) {
+        arr[index3] = arr_2[index2];
+        ++index3;
+    }
+    
+    
+    std::cout << "SORTED ARRAY:\n";
+    for (int i = l; i < r; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    
+    std::cout << "\n";
+    
+    
+    return arr;
+}
+
+void mergesort(int * arr, int l, int r) {
+    
+    std::cout << "in mergesort\n";
+    
+    if (l < r) {
+        // first half l to m-1
+        // second half m to r - 1?
+        
+        int m = (l + (r - 1)) / 2;
+        mergesort(arr, l, m);
+        mergesort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
+}
+
+
+
+
+
+int main () {
+    
+    int test[5];
+    
+    for (int i = 0; i < 5; ++i) {
+        test[i] = 5 - i;
+    }
+    
+    mergesort(test, 0, 5);
+    
+    for (int i = 0; i < 5; ++i) {
+        std::cout << test[i] << " \n" << std::endl;
+    }
+    
+    return 0;
+}
+
---
+++ quick_sort.cpp
@@ -0,0 +1,61 @@
+// INSERTION SORT - cycle through array, inserting elements in correct position
+// swap towards left until element is in the correct spot
+// O(n^2)
+
+#include <iostream>
+#include <vector>
+
+int partition(int * arr, int l, int r) {
+    
+    // set i and j
+    int i = l;
+    int pivot = (l + r) / 2;
+    
+    // flip pivot and last element
+    int pivot_val = arr[pivot];
+    arr[pivot] = arr[r];
+    arr[r] = pivot_val;
+    
+    for (int j = l + 1, j < r - 1; ++j) {
+        if (arr[j] < pivot_val) {
+            // flip i and j
+            int temp = arr[j];
+            arr[j] = arr[i];
+            arr[i] = temp;
+            ++i;
+        }
+    }
+    
+    // flip pivot and correct place
+    int i_val = arr[i + 1];
+    arr[i + 1] = arr[r];
+    arr[r] = i_val;
+    
+    return i + 1;
+    
+    
+}
+
+
+
+void quicksort(int * arr, int l, int r) {
+    if (l < r) {
+        int p = partition(arr, l, r);
+        quicksort(arr, l, p);
+        quicksort(arr, p + 1, r);
+    }
+}
+
+
+
+
+int main() {
+    int test[5];
+    test[0] = 3;
+    test[1] = 4;
+    test[2] = 0;
+    test[3] = 2;
+    test[4] = 1;
+    
+    return 0;
+}
